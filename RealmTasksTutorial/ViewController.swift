62450002326bd2527f9798aa03dad1a3230f613a
andrew glew <drew@andrews-MacBook-Pro.local>
2017-01-05 20:11:37 +0100

    first update

--- RealmTasksTutorial/ViewController.swift
+++ RealmTasksTutorial/ViewController.swift
@@ -1,25 +1,4 @@
-//
-//  ViewController.swift
-//  RealmTasksTutorial
-//
-//  Created by andrew glew on 05/01/2017.
-//  Copyright © 2017 andrew glew. All rights reserved.
-//
-
 import UIKit
 
-class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
+class ViewController: UITableViewController {
 }
-
